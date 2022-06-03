479e76dec808f3dd635d14cbe34a9db4897fd89c
elselovezml <zmlelselove@126.com>
2022-06-03 20:39:27 +0800

    第四题题解

---
+++ src/main/java/code/hard/q4/Solution.java
@@ -0,0 +1,84 @@
+package code.hard.q4;
+
+
+import test.TestAble;
+
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+/**
+ *
+ * 寻找两个正序数组的中位数
+ */
+public class Solution implements TestAble {
+    @Override
+    public Method getTestMethod() {
+        try {
+            return this.getClass().getMethod("findMedianSortedArrays", int[].class, int[].class);
+        } catch (NoSuchMethodException e) {
+            return null;
+        }
+    }
+
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int l1 = nums1.length;
+        int l2 = nums2.length;
+        int totalNum = l1 + l2;
+        boolean isOdds = totalNum % 2 == 1;
+        int endSize = totalNum / 2;
+        int index = 0;
+        IndexRecord indexRecord = new IndexRecord(l1, l2);
+        while(index < endSize - 1) {
+            index ++;
+            nextMinValue(nums1, nums2,indexRecord);
+        }
+        // 奇数
+        if (isOdds) {
+            if (l1 + l2 == 1) {
+                return nextMinValue(nums1, nums2,indexRecord);
+            }
+            nextMinValue(nums1, nums2,indexRecord);
+            return nextMinValue(nums1, nums2,indexRecord);
+        } else {
+            return (nextMinValue(nums1, nums2,indexRecord) + nextMinValue(nums1, nums2,indexRecord)) / 2.0;
+        }
+    }
+
+    private int nextMinValue(int[] nums1, int[] nums2, IndexRecord record) {
+        // 有一个数组遍历结束
+        if (record.i > record.l1 - 1) {
+            record.j ++;
+            return nums2[record.j - 1];
+        }
+        if (record.j > record.l2 - 1) {
+            record.i ++;
+            return nums1[record.i - 1];
+        }
+        if (nums1[record.i] > nums2[record.j]) {
+            record.j ++;
+            return nums2[record.j - 1];
+        } else {
+            record.i ++;
+            return nums1[record.i - 1];
+        }
+    }
+
+    static class IndexRecord {
+        final int l1;
+        final int l2;
+        int i;
+        int j;
+
+        public IndexRecord(int l1, int l2) {
+            this.l1 = l1;
+            this.l2 = l2;
+            i = 0;
+            j = 0;
+        }
+    }
+
+}
--- src/main/java/test/TestMain.java
+++ src/main/java/test/TestMain.java
@@ -1,29 +1,23 @@
 package test;
 
-import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
 public class TestMain {
-    private static final String TYPE = "normal";
-    private static final int QUESTION_NO = 3;
+    private static final String TYPE = "hard";
+    private static final int QUESTION_NO = 4;
     private static final String CLASS_NAME = String.format("code.%s.q%d.Solution", TYPE, QUESTION_NO);
 
-    private static Executable getExecuteAbleInstance() throws ClassNotFoundException,
-            InstantiationException,
-            IllegalAccessException {
+    private static Executable getExecuteAbleInstance() throws Exception {
         Class<?> solutionClass = Class.forName(CLASS_NAME);
-        Object instance = solutionClass.newInstance();
+        Object instance = solutionClass.getDeclaredConstructor().newInstance();
         TestAble testInstance = (TestAble) instance;
         Method method = testInstance.getTestMethod();
         return new Executable(instance, method, new Object[] {
-                "bbtablud"
+                new int[] {1}, new int[]{}
         });
     }
 
-    public static void main(String[] args) throws ClassNotFoundException,
-            InstantiationException,
-            IllegalAccessException,
-            InvocationTargetException {
+    public static void main(String[] args) throws Exception {
         long b = System.currentTimeMillis();
         Executable executable = getExecuteAbleInstance();
         Object result = executable.method.invoke(executable.instance, executable.args);
